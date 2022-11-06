ea8756eb75770af5de17b252f45b021bf4c78a1e
Tristan <74681513+AstralsDB@users.noreply.github.com>
2022-11-06 02:56:13 -0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,9 @@
+module AstralScan
+
+go 1.18
+
+require (
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/zan8in/masscan v0.0.0-20221031084225-ef31fffa7df6 // indirect
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/zan8in/masscan"
+)
+
+func main() {
+	context, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+
+	var (
+		scannerResult []masscan.ScannerResult
+		errorBytes    []byte
+	)
+
+	scanner, err := masscan.NewScanner(
+		masscan.SetParamTargets(os.Args[1]),
+		masscan.SetParamPorts("0-9999,u0-9999"),
+		masscan.SetParamExclude("255.255.255.255"),
+		masscan.SetParamWait(0),
+		masscan.SetParamRate(100000000),
+		masscan.WithContext(context),
+	)
+
+	if err != nil {
+		log.Fatalf("unable to create masscan scanner: %v", err)
+	}
+
+	if err := scanner.RunAsync(); err != nil {
+		panic(err)
+	}
+
+	stdout := scanner.GetStdout()
+	stderr := scanner.GetStderr()
+
+	go func() {
+		fmt.Printf("Address\t\tPort\n")
+
+		for stdout.Scan() {
+			srs := masscan.ParseResult(stdout.Bytes())
+			// fmt.Println(srs.IP, srs.Port)
+			fmt.Printf("%s\t%v\n", srs.IP, srs.Port)
+			scannerResult = append(scannerResult, srs)
+		}
+	}()
+
+	go func() {
+		for stderr.Scan() {
+			// fmt.Println("err: ", stderr.Text())
+			errorBytes = append(errorBytes, stderr.Bytes()...)
+		}
+	}()
+
+	if err := scanner.Wait(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v Hosts Found\n", len(scannerResult))
+	fmt.Printf("PID %v Exited", scanner.GetPid())
+}
